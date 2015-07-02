e401d3335f43250f4cf0472e6ea8d5463f60829d
jenjiyi <jenny.yamada@gmail.com>
2015-07-01 18:30:24 -0700

    validation

--- js/app.js
+++ js/app.js
@@ -31,9 +31,11 @@ $(document).ready(function(){
   /*--- clear game ---*/
   function clearFields(){
         $('#feedback').empty().html("Make your Guess!");
+        $('#validate').empty();
         $('#count').empty();
         $('#guessList').empty();
         $("#userGuess").prop("disabled", false).prop("placeholder", "Enter your Guess");
+        $("#guessButton").prop("disabled", false);
           }
   /*--- count guesses ---*/
   function guessCount(){
@@ -45,6 +47,32 @@ function guessLog(x){
   //print guesses in <li> to ul#guessList 
   $('#guessList').append("<li>"+ x +"</li>");
 }
+//display guess feedback
+function feedback(d){
+  if (d >= 50){
+    $("#feedback").html("You're ice cold. Guess again.");
+  }
+  else if (d >= 30 ){
+        $("#feedback").html("You're cold. Guess again.");
+  }
+  else if (d >= 20 ){
+        $("#feedback").html("You're warm. Guess again.");
+  }
+  else  if (d >= 11) {
+        $("#feedback").html("You're hot. Guess again.");
+  }
+  else if (d >= 1) {
+        $("#feedback").html("You're very hot. Guess again.");
+      }
+  else {
+    $("#feedback").html("You're right. Congratulations!");
+    $("#userGuess").prop("disabled", true).prop("placeholder", "Woohoo!");
+    $("#guessButton").prop("disabled", true);
+  };
+  //$('#feedback').empty().html("Make your Guess!");
+  $('#userGuess').val("");
+
+  };
 
 	/*--- start new game ---*/
 	function newGame(){
@@ -56,50 +84,24 @@ function guessLog(x){
 		  //getValue of input field #userGuess on button click #guessButton store as guessValue
         $('#guessButton').on('click', function(e){
           e.preventDefault();
+          $("#validate").empty();
           var getVal =+ $('#userGuess').val();
-    
            //Checks if the typed value is an integer from 1-100
           if (getVal % 1 != 0 || getVal > 100 || getVal < 1  || isNaN(getVal)){
           //alert('Type only numbers between 1 and 100.');
-          $("#feedback").html("Type only numbers between 1 and 100.");
+          $("#validate").html("Guess only numbers between 1 and 100.");
             $('#userGuess').val("");
      
            }else{
+            
             var difference = diff(getVal, secretNumber);
-              console.log(difference);
+            console.log(difference);
             feedback(difference);
             guessLog(getVal);
             guessCount();
           }
         });
-	}	
+	};	
   		
-//display guess feedback
-function feedback(d){
-  //append to div#feedback
-  if (d >= 50){
-    $("#feedback").html("You're ice cold. Guess again.");
-  }
-  else if (d >= 30 ){
-		    $("#feedback").html("You're cold. Guess again.");
-  }
-  else if (d >= 20 ){
-        $("#feedback").html("You're warm. Guess again.");
-  }
-  else  if (d >= 11) {
-        $("#feedback").html("You're hot. Guess again.");
-  }
-  else if (d >= 1) {
-        $("#feedback").html("You're very hot. Guess again.");
-      }
-	else {
-    $("#feedback").html("You're right. Congratulations!");
-    $("#userGuess").prop("disabled", true).prop("placeholder", "Woohoo!");
-    $("#guessButton").prop("disabled", true);
-  };
-  //$('#feedback').empty().html("Make your Guess!");
-  $('#userGuess').val("");
-
-  }
 
 });
